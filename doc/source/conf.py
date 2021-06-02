5bcdbee24dd595b6ceda5477b64d0d0a423fe71c
RaphaelWeber <raphael.weber@univ-rennes1.fr>
2021-06-02 08:31:11 +0200

    main - update documentation configuration

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -16,29 +16,43 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-text_top = "The package **visiannot** is made up of three sub-packages:\n"
+from os.path import abspath
+import sys
+sys.path.insert(0, abspath('.'))
 
-text_bottom = """
-The sub-package **configuration** contains modules where are defined the
-classes for the creation of the configuration GUI.
 
-The sub-package **tools** contains modules with functions that may be used
-outside of **ViSiAnnoT**. In particular, the modules **ToolsPyQt** and
-**ToolsPyqtgraph** are an overlayer of **PyQt5** and **pyqtgraph**
-respectively. They may be used in order to ease the creation of a GUI and
-scientific graphics respectively. See chapters :ref:`toolspyqt` and
-:ref:`toolspyqtgraph`.
+### in order to automatically generate APIreference index files,
+# this block must be uncommented and the documentation must be generated
+# locally, then re-comment this block and build the documentation on
+# ReadTheDocs
+###
 
-The sub-package **visiannot** contains modules where are defined the classes
-for the creation of the ViSiAnnoT GUI.
+# text_top = "The package **visiannot** is made up of three sub-packages:\n"
 
-The summary of the modules can be found at the top of their respective page.
-"""
+# text_bottom = """
+# The sub-package **configuration** contains modules where are defined the
+# classes for the creation of the configuration GUI.
 
-from autoDocAPI import generateIndexFiles
-generateIndexFiles(
-    "visiannot", "../..", text_top=text_top, text_bottom=text_bottom
-)
+# The sub-package **tools** contains modules with functions that may be used
+# outside of **ViSiAnnoT**. In particular, the modules **ToolsPyQt** and
+# **ToolsPyqtgraph** are an overlayer of **PyQt5** and **pyqtgraph**
+# respectively. They may be used in order to ease the creation of a GUI and
+# scientific graphics respectively. See chapters :ref:`toolspyqt` and
+# :ref:`toolspyqtgraph`.
+
+# The sub-package **visiannot** contains modules where are defined the classes
+# for the creation of the ViSiAnnoT GUI.
+
+# The summary of the modules can be found at the top of their respective page.
+# """
+
+# from autoDocAPI import generateIndexFiles
+# generateIndexFiles(
+#     "visiannot", "../..", text_top=text_top, text_bottom=text_bottom
+# )
+
+###
+###
 
 from summaryGroups import setPackageName
 setPackageName("visiannot")
