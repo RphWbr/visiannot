d3f1f480501afc1142de80e2e591c82b1fcfe009
RaphaelWeber <raphael.weber@univ-rennes1.fr>
2021-06-03 14:58:13 +0200

    add script for automatically creating API reference index files

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -21,48 +21,10 @@ import sys
 
 path = dirname(abspath(__file__))
 sys.path.insert(0, abspath('.'))
-# sys.path.insert(0, abspath(join(path, '..', '..')))
+sys.path.insert(0, abspath(join(path, '..', '..')))
 
 from visiannot import __version__ as version
 from datetime import datetime
-
-
-### in order to automatically generate APIreference index files,
-# this block must be uncommented and the documentation must be generated
-# locally, then re-comment this block and build the documentation on
-# ReadTheDocs
-###
-
-# text_top = "The package **visiannot** is made up of three sub-packages:\n"
-
-# text_bottom = """
-# The sub-package **configuration** contains modules where are defined the
-# classes for the creation of the configuration GUI.
-
-# The sub-package **tools** contains modules with functions that may be used
-# outside of **ViSiAnnoT**. In particular, the modules **ToolsPyQt** and
-# **ToolsPyqtgraph** are an overlayer of **PyQt5** and **pyqtgraph**
-# respectively. They may be used in order to ease the creation of a GUI and
-# scientific graphics respectively. See chapters :ref:`toolspyqt` and
-# :ref:`toolspyqtgraph`.
-
-# The sub-package **visiannot** contains modules where are defined the classes
-# for the creation of the ViSiAnnoT GUI.
-
-# The summary of the modules can be found at the top of their respective page.
-# """
-
-# from autoDocAPI import generateIndexFiles
-# generateIndexFiles(
-#     "visiannot", "../..", text_top=text_top, text_bottom=text_bottom
-# )
-
-###
-###
-
-from summaryGroups import setPackageName
-setPackageName("visiannot")
-from summaryGroups import setup
 
 
 # -- Project information -----------------------------------------------------
@@ -75,8 +37,7 @@ author = 'Raphael Weber'
 release = version
 
 # The short X.Y version
-# version = '.'.join(version.split('.')[:2])
-version = '0.0.10'
+version = '.'.join(version.split('.')[:2])
 
 
 # -- General configuration ---------------------------------------------------
