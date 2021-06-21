242b4bb328a43594a7d28f088c0b40fd4baf5688
RaphaelWeber <raphael.weber@univ-rennes1.fr>
2021-06-21 16:36:23 +0200

    __main__ script add optional argument for specifying a configuration file path

--- visiannot/__main__.py
+++ visiannot/__main__.py
@@ -1,6 +1,27 @@
 # -*- coding: utf-8 -*-
 
 from visiannot.visiannot import ViSiAnnoTLongRecFromConfigGUI
+from argparse import ArgumentParser
+from os.path import abspath
 
 if __name__ == '__main__':
-    win_visiannot = ViSiAnnoTLongRecFromConfigGUI()
+    #############
+    # arguments #
+    #############
+    parser = ArgumentParser()
+
+    parser.add_argument(
+        "--config_path",
+        "-conf",
+        type=str,
+        help="path to the configuration file, default ''",
+        default=''
+    )
+
+    args = parser.parse_known_args()
+    config_path = abspath(args[0].config_path)
+
+    ####################
+    # launch ViSiAnnoT #
+    ####################
+    win_visiannot = ViSiAnnoTLongRecFromConfigGUI(path=config_path)
