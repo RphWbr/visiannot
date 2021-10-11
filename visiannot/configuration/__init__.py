5625594b3876a2ae37a03e0261cd6c65116df609
RphWbr <raphael.weber@univ-rennes1.fr>
2021-10-11 12:32:44 +0200

    launcher - update __init__ of visiannot.configuration, better generalization when adding new modules

--- visiannot/configuration/__init__.py
+++ visiannot/configuration/__init__.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from os.path import dirname
+from pkgutil import iter_modules
+
+# import classes to ease import outside of the package
 from .Configuration import Configuration
 from .ConfigurationWindow import ConfigurationWindow
 
@@ -8,7 +11,6 @@ from .ConfigurationWindow import ConfigurationWindow
 pkg_dir = dirname(__file__)
 __path__ = [pkg_dir]
 
-__all__ = [
-    "Configuration",
-    "ConfigurationWindow"
-]
+__all__ = []
+for _, module_name, flag_pkg in iter_modules(__path__):
+    __all__.append(module_name)
