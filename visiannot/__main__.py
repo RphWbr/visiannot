1d7a29b28eb9502040a23c0c7c567a129c305028
RphWbr <raphael.weber@univ-rennes1.fr>
2021-10-11 09:45:14 +0200

    launcher - update __main__.py with different behaviors (conf GUI + no visi GUI / no conf GUI + visi GUI / conf GUI + visi GUI)

--- visiannot/__main__.py
+++ visiannot/__main__.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
+#
+# Copyright Université Rennes 1 / INSERM
+# Contributor: Raphael Weber
+#
+# Under CeCILL license
+# http://www.cecill.info
 
-from visiannot.visiannot import ViSiAnnoTLongRecFromConfigGUI, ViSiAnnoTLongRecFromConfigFile
+from visiannot.visiannot import ViSiAnnoTLongRecFromConfigFile, \
+    ViSiAnnoTLongRecFromConfigGUI
+from visiannot.configuration import ConfigurationWindow
 from argparse import ArgumentParser
 from os.path import abspath
 
@@ -20,22 +28,42 @@ if __name__ == '__main__':
 
     parser.add_argument(
         "--flag_config_gui",
-        "-fcg",
+        "-fconf",
         type=int,
         help="boolean (0 or 1) to specify if configuration GUI must be launched, default 1",
+        default=1
+    )
+
+    parser.add_argument(
+        "--flag_visi_gui",
+        "-fvisi",
+        type=int,
+        help="boolean (0 or 1) to specify if ViSiAnnoT GUI must be launched, default 1",
         default=1
     )
 
     args = parser.parse_known_args()
     config_path = abspath(args[0].config_path)
     flag_config_gui = args[0].flag_config_gui
+    flag_visi_gui = args[0].flag_visi_gui
 
     ####################
     # launch ViSiAnnoT #
     ####################
 
+    # check if configuration GUI
     if flag_config_gui:
-        win_visiannot = ViSiAnnoTLongRecFromConfigGUI(path=config_path)
+        # check if ViSiAnnoT GUI
+        if flag_visi_gui:
+            win_visi = ViSiAnnoTLongRecFromConfigGUI(path=config_path)
+
+        else:
+            # only configuration GUI
+            win_config = ConfigurationWindow(path=config_path)
+
+    elif flag_visi_gui:
+        # only ViSiAnnoT GUI
+        win_visi = ViSiAnnoTLongRecFromConfigFile(config_path)
 
     else:
-        win_visiannot = ViSiAnnoTLongRecFromConfigFile(config_path)
+        print("Nothing to do!")
