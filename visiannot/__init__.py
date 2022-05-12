ed7e02d5cf2097be848dedac97ea562aea299253
RphWbr <raphael.weber@univ-rennes1.fr>
2022-05-12 15:12:08 +0200

    VERSIONING

--- visiannot/__init__.py
+++ visiannot/__init__.py
@@ -8,7 +8,7 @@ Initially developed as part of the european project Digi-NewB at the LTSI lab,
 Université Rennes 1.
 """
 
-__version__ = "0.2.8"
+__version__ = "0.3.0"
 __authors__ = "Raphaël Weber"
 
 
