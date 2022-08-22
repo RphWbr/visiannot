65a700ead34b1c3cbbaa288c9ad211f686c0c38e
RphWbr <raphael.weber@univ-rennes1.fr>
2022-08-22 10:50:13 +0200

    integrate script for creating fake annotations DURATION inside configuration.update

--- visiannot/configuration/update.py
+++ visiannot/configuration/update.py
@@ -15,6 +15,55 @@ launching GUI
 from . import ConfigurationWindow
 from ..tools.pyqt_overlayer import get_directory_dialog
 from os.path import basename
+from glob import glob
+from os.path import isdir, isfile
+from os import makedirs, remove
+from ..tools.datetime_converter import get_datetime_from_path
+from ..tools.video_loader import get_duration_video
+from datetime import timedelta
+
+
+def get_rec_video_duration(video_dir, output_dir):
+    """
+    Creates automatically the fake annotations "DURATION" with the duration of
+    video files in the long recording
+
+    :param video_dir: directory where to find the videos of the long recording
+    :type video_dir: str
+    :param output_dir: directory where to save fake annotations
+    :type output_dir: str
+    """
+
+    timestamp_format = "%Y-%m-%dT%H:%M:%S.%f"
+
+    # get list of video paths
+    vid_path_list = sorted(glob("%s/*BW1*.mp4" % video_dir))
+
+    # get output path
+    base_name_split = basename(vid_path_list[0]).split('_')
+    output_path = "%s/%s_%s_DURATION-datetime.txt" % (
+        output_dir, base_name_split[0], base_name_split[1]
+    )
+
+    if isfile(output_path):
+        remove(output_path)
+
+    # loop on paths
+    for path in vid_path_list:
+        beg_datetime = get_datetime_from_path(
+            path, '_', 1, "%Y-%m-%dT%H-%M-%S"
+        )
+        duration = get_duration_video(path)
+
+        end_datetime = beg_datetime + timedelta(seconds=duration)
+
+        # convert datetime to string
+        beg_string = beg_datetime.strftime(timestamp_format)
+        end_string = end_datetime.strftime(timestamp_format)
+
+        # write annotation file
+        with open(output_path, 'a') as f:
+            f.write("%s - %s\n" % (beg_string, end_string)) 
 
 
 def update_data_and_annotations_directory(config_path):
@@ -75,8 +124,17 @@ def update_data_and_annotations_directory(config_path):
     # get patient id
     rec_name = basename(dir_data)
     pat_id = rec_name.split("_")[0]
-    config_dict["General"]["annot_dir"] = "%s/%s/%s" % (
+
+    # update annotation directory
+    annot_dir = "%s/%s/%s" % (
         config_dict["General"]["annot_dir"], pat_id, rec_name
     )
+    config_dict["General"]["annot_dir"] = annot_dir
+
+    if not isdir(annot_dir):
+        makedirs(annot_dir)
+
+        # create fake annotation "DURATION"
+        get_rec_video_duration(dir_data, annot_dir)
 
     return config_dict
